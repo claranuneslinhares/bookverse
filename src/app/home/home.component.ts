6d680a62c2d7cfec73cbb4c09fa0aa600e4a3e21
Maria Clara Nunes Linhares <20241148060036@ifrn.local>
2025-01-30 10:06:01 -0300

    Criando html e fazendo configuração

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../services/book.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [OnInit, BookService],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  implements OnInit {
+    books[ ];
+    constructor(private bookService: BookService) {}
+    ngOninit(){
+      this.bookService.getbooks()subscribeOn((data) => {
+        this.books=data;
+      });
+      }
+    }
 }
+
